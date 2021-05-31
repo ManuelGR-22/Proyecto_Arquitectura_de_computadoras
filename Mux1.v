a20046db1f7239107c958122e0a357bdfc26f908
Athziri-Rubio <84393510+Athziri-Rubio@users.noreply.github.com>
2021-05-31 16:52:01 -0500

    Update Mux1.v

--- Mux1.v
+++ Mux1.v
@@ -1,14 +1,12 @@
 `timescale 1ns/1ns
 //1.Creacion del modulo 
 //Declaracion del modulo con sus entradas y salidas 
-//Definir constante E de 32 
-module MUX#(parameter E = 32)
-	(
+module mux1(
 	input MemTR,
-	input [E-1:0] _0_in,
-	input [E-1:0] _1_in,
-	output reg [E-1:0] DW
-	);
+	input [4:0] C5,
+	input [4:0] C3,
+	output reg [4:0] DW
+);
 //declaracion de señales y variables 
 	//NA
 //2.Declaraciones
@@ -19,19 +17,16 @@ module MUX#(parameter E = 32)
 //instancias
 	//N/A
 //Bloques always (continuo)
-	always @(*)
-		begin
-		
-		//asignamiento bloqueante ya que si importa el oreden
-		//de las asignaciones ya que las acciones se ejecutan de forma secuencial
-			DW= 32'h00000000; //equivalencia a 32 bits en binario
-			if(MemTR)
-				begin
-					DW = _1_in;//ESCRIBIR
-				end
-			else
-				begin
-					DW = _0_in;//LEER
-				end
-		end
+
+always@*
+begin
+	case(MemTR)
+		1'd1:
+			DW = C3;//Escribir
+		1'd0:
+			DW = C5;//Leer
+
+	endcase
+end
 endmodule
+
