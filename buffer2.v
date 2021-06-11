b2d0896c3b18f1c7cd617cb7ddeeae773a39af46
Athziri-Rubio <84393510+Athziri-Rubio@users.noreply.github.com>
2021-06-10 22:56:41 -0500

    Update buffer2.v

--- buffer2.v
+++ buffer2.v
@@ -3,43 +3,64 @@
 //Declaracion del modulo con sus entradas y salidas 
 module buffer2
 	(
+	//UNIDAD DE CONTROL
+	//ENTRADAS
+	//WB
+	input INB2MemREG,
+	input INB2RegWRITE,
+	//M
+	input INB2Branch,
+	input INB2MemWRITE,
+	input INB2MemRead,
+	//EX
+	input INB2RegDst,
+	input [2:0]INB2ALUOP,
+	input INB2ALUSrc,
+	//RESTO
 	input clk,
-	input [31:0] inputAdd,
+	input [31:0] inputAddB2,
   	input [31:0] inputRD1,
 	input [31:0] inputRD2,
 	input [31:0] inputSinex,
 	input [4:0] inputRr1,
 	input [4:0] inputRr2,
-	input [31:0] inputRd1,
-	output reg [31:0] outputAdd,
+	//UNIDAD DE CONTROL
+	//SALIDAS
+	//WB
+	output reg OTB2MemREG,
+	output reg OTB2RegWRITE,
+	//M
+	output reg OTB2Branch,
+	output reg OTB2MemWRITE,
+	output reg OTB2MemRead,
+	//EX
+	output reg OTB2RegDst,
+	output reg [2:0]OTB2ALUOP,
+	output reg OTB2ALUSrc,
+	//RESTO
+	output reg [31:0] outputAddB2,
 	output reg [31:0] outputRD1,
 	output reg [31:0] outputRD2,
 	output reg [31:0] outputSinex,
-	output reg [31:0] outputRr1,
-	output reg [31:0] outputRr2,
-  	output reg [31:0] outputInsRD1
+	output reg [4:0] outputRr1,
+	output reg [4:0] outputRr2
 	);
-//declaracion de señales y variables 
-	//NA
-//2.Declaraciones
-	//N/A
-//3.Cuerpo del modulo--------
-// asignaciones
-	//NA	
-//instancias
-	//N/A
-//Bloques always 
-/*Deteccion de flanco: sirve para que un proceso solo se ejecute 
-en determinados flancos de reloj de una o mas señales de entrada*/
-//posedge detecta el flanco de subida
+
 	always @(posedge clk)
 	begin
-		outputAdd = inputAdd;
+		OTB2MemREG = INB2MemREG;
+		OTB2RegWRITE = INB2RegWRITE;
+		OTB2Branch = INB2Branch;
+		OTB2MemWRITE = INB2MemWRITE;
+		OTB2MemRead = INB2MemRead;
+		OTB2RegDst = INB2RegDst;
+		OTB2ALUOP = INB2ALUOP;
+		OTB2ALUSrc = INB2ALUSrc;
+		outputAddB2 = inputAddB2;
 		outputRD1 = inputRD1;
 		outputRD2 = inputRD2;
 		outputSinex = inputSinex;
 		outputRr1 = inputRr1;
 		outputRr2 = inputRr2;
-		outputInsRD1 = outputInsRD1;
 	end
 endmodule
