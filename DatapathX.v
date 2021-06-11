bb5143f0537a95d9788c22e5bceeff329052fcb3
Athziri-Rubio <84393510+Athziri-Rubio@users.noreply.github.com>
2021-06-10 22:47:39 -0500

    Update DatapathX.v

--- DatapathX.v
+++ DatapathX.v
@@ -5,42 +5,130 @@ module singledptR(
 
 );
 
-	wire MemREG, RegWRITE, MemWRITE, Branch, MemRead, ALUSrc, RegDst, Z_flag, Z;
-	wire [2:0] ALUOP;
-	wire [4:0] c13;
-	wire [2:0] OP;
-	wire [31:0] c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c14;
+	//BUFFER 1 IF/ID
+	wire [31:0] inputAdd;
+  	wire [31:0] inputInsMem;
+	wire [31:0] outputAdd;
+  	wire [31:0] OutputInsMem;
+  	//BUFFER 2 ID/EX
+  	//UNIDAD DE CONTROL
+	//ENTRADAS
+	//WB
+	wire INB2MemREG;
+	wire INB2RegWRITE;
+	//M
+	wire INB2Branch;
+	wire INB2MemWRITE;
+	wire INB2MemRead;
+	//EX
+	wire INB2RegDst;
+	wire [2:0]INB2ALUOP;
+	wire INB2ALUSrc;
+	//RESTO
+  	wire [31:0] inputRD1;
+	wire [31:0] inputRD2;
+	wire [31:0] inputSinex;
+	//UNIDAD DE CONTROL
+	//SALIDAS
+	//WB
+	wire OTB2MemREG;
+	wire OTB2RegWRITE;
+	//M
+	wire OTB2Branch;
+	wire OTB2MemWRITE;
+	wire OTB2MemRead;
+	//EX
+	wire OTB2RegDst;
+	wire [2:0]OTB2ALUOP;
+	wire OTB2ALUSrc;
+	//RESTO
+	wire [31:0] outputAddB2;
+	wire [31:0] outputRD1;
+	wire [31:0] outputRD2;
+	wire [31:0] outputSinex;
+	wire [4:0] outputRr1;
+	wire [4:0] outputRr2;
+  	//BUFFER 3 EX/MEM
+	//RESTO
+	wire [31:0] inputAddB3;
+	wire Z_flag;
+  	wire [31:0] inputAddrst;
+	wire [4:0] inputmux;
+	//UNIDAD DE CONTROL
+	//SALIDAS
+	//WB
+	wire OTB3MemREG;
+	wire OTB3RegWRITE;
+	//M
+	wire OTB3Branch;
+	wire OTB3MemWRITE;
+	wire OTB3MemRead;
+	//RESTO
+	wire [31:0] outputAddB3;
+	wire [31:0] outputAddrst;
+	wire [31:0] outputRD2B3;
+	wire output_Z_flag;
+	wire [4:0] outputmux;
+	//BUFFER 4 MEM/WB
+	//RESTO
+	wire [31:0] inputDatmem;
+	//UNIDAD DE CONTROL
+	//SALIDAS
+	//WB
+	wire OTB4MemREG;
+	wire OTB4RegWRITE;
+	//RESTO
+	wire [31:0] outputDatmem;
+	wire [31:0] outputAddrstB4;
+	wire [4:0] outputmuxB4;
+	//SIN BUFFER
+	wire [31:0] PcInsMemYAdd;
+	wire [31:0] MuxPc;
+	wire [31:0] SL2Add;
+	wire [31:0] MuxALU;
+	wire [2:0] ALUCtrlALU;
+	wire [31:0] MuxBR;
+	wire PCSrc;
 
-	unit_control ucontrol(c2[31:26], MemREG, RegWRITE, MemWRITE, Branch, MemRead, ALUSrc, RegDst, ALUOP);
+	buffer1 b1(clk,inputAdd,inputInsMem,outputAdd,OutputInsMem);
 
-	BankReg regb(c2[25:21], c2[20:16], c13, RegWRITE, c12, c3, c4);
+	buffer2 b2(INB2MemREG,INB2RegWRITE,INB2Branch,INB2MemWRITE,INB2MemRead,INB2RegDst,INB2ALUOP,INB2ALUSrc,clk,outputAdd,inputRD1,inputRD2,inputSinex,OutputInsMem[20:16],OutputInsMem[15:11],OTB2MemREG,OTB2RegWRITE,OTB2Branch,OTB2MemWRITE,OTB2MemRead,OTB2RegDst,OTB2ALUOP,OTB2ALUSrc,outputAddB2,outputRD1,outputRD2,outputSinex,outputRr1,outputRr2);
 
-	alu_ctrl_x aluctrl(c2[5:0], ALUOP, OP);
+	buffer3 b3(OTB2MemREG,OTB2RegWRITE,OTB2Branch,OTB2MemWRITE,OTB2MemRead,clk,inputAddB3,Z_flag,inputAddrst,outputRD2,inputmux,OTB3MemREG,OTB3RegWRITE,OTB3Branch,OTB3MemWRITE,OTB3MemRead,outputAddB3,outputAddrst,outputRD2B3,output_Z_flag,outputmux);
 
-	insmemx insm(c1, c2);
+	buffer4 b4(OTB3MemREG,OTB3RegWRITE,clk,inputDatmem,outputAddrst,outputmux,OTB4MemREG,OTB4RegWRITE,outputDatmem,outputAddrstB4,outputmuxB4);
 
-	Suma_4b sumador (c1, 32'd4, c9);
+	unit_control ucontrol(OutputInsMem[31:26],INB2MemREG,INB2RegWRITE,INB2MemWRITE,INB2Branch,INB2MemRead,INB2ALUSrc,INB2RegDst,INB2ALUOP);
 
-	Suma_4b sumador2 (c9, c8, c10);
+	BankReg regb(OutputInsMem[25:21],OutputInsMem[20:16],outputmuxB4,OTB4RegWRITE,MuxBR,inputRD1,inputRD2);
 
-	pc pc(c11, clk, c1);
+	alu_ctrl aluctrl(outputSinex[5:0],OTB2ALUOP,ALUCtrlALU);
 
-	alu_x operator (c3, c14, OP, c6, Z_flag);
+	insmemx insm(PcInsMemYAdd,inputInsMem);
 
-	mem_x memory (c4, c6, MemWRITE, MemRead, c7);
+	Suma_4b sumador(PcInsMemYAdd,32'd4,inputAdd);
 
-	mux1 mux1 (RegDst, c2[15:11], c2[20:16], c13);
+	Suma_4b sumador2(outputAddB2,SL2Add,inputAddB3);
 
-	mux2 mux2 (ALUSrc, c5, c4, c14);
+	pc pc(MuxPc,clk,PcInsMemYAdd);
 
-	mux2 mux3 (Z, c10, c9, c11);
+	ALU operator(outputRD1,MuxALU,ALUCtrlALU,inputAddrst,Z_flag);
 
-	mux2 mux4 (MemREG, c7, c6, c12);
+	mem_x memory(outputAddrst,outputRD2B3,OTB3MemWRITE,OTB3MemRead,inputDatmem);
 
-	MAnd andy(Branch, Z_flag, Z);
+	mux1 mux1(OTB2RegDst,outputRr1,outputRr2,inputmux);
 
-	shiftL shift(c5, c8);
+	mux2 mux2(PCSrc,outputAddB3,inputAdd,MuxPc);
 
-	signextend sign (c2[15:0], c5);
+	mux2 mux3(OTB2ALUSrc,outputSinex,outputRD2,MuxALU);
+
+	mux2 mux4(OTB4MemREG,outputDatmem,outputAddrstB4,MuxBR);
+
+	MAnd andy(OTB3Branch,output_Z_flag,PCSrc);
+
+	shiftL shift(outputSinex,SL2Add);
+
+	signextend sign(OutputInsMem[15:0],inputSinex);
 
 endmodule
+
