a7166a55f4c9f669e3e1f853cbffca2471993d3d
Athziri-Rubio <84393510+Athziri-Rubio@users.noreply.github.com>
2021-06-10 22:49:33 -0500

    Update InsMemX.v

--- InsMemX.v
+++ InsMemX.v
@@ -3,15 +3,17 @@ module insmemx (
 	input [31:0] Dir,
 	output reg [31:0] instruction
 );
-reg[7:0] insMemory [0:399];
+reg [7:0] insMemory [0:399];
 
 initial
+begin
 	$readmemb("TestF2_MemInst.mem",insMemory);
-
+end
+	
 always @*
 begin
 	
 	instruction = {insMemory[Dir], insMemory[Dir+1], insMemory[Dir+2], insMemory[Dir+3]};
-
 end
 endmodule
+
