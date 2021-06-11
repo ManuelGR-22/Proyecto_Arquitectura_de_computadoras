34676fcd04bc18ae1a61448695d893c39d72148b
Athziri-Rubio <84393510+Athziri-Rubio@users.noreply.github.com>
2021-06-10 22:46:50 -0500

    Update BankRegister.v

--- BankRegister.v
+++ BankRegister.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ns
 //1.Creacion del modulo 
 module BankReg(
-input[4:0] AR1, //Read Register 1
+	input[4:0] AR1, //Read Register 1
 	input[4:0] AR2, //Read Register 2
 	input[4:0] AW,  
 	input REG_WRITE, //RegWrite
@@ -10,13 +10,18 @@ input[4:0] AR1, //Read Register 1
 	output reg [31:0] DR2  //Read data 2
 );
 reg [31:0] memory [0:31];
-initial $readmemb("TestF1_BReg.txt",memory);
-
+initial 
+begin
+	$readmemb("TestF1_BReg.mem", memory);
+end
 always @*
 begin
 //Antes de leer escribir a eso se refiere
-	if(REG_WRITE) memory[AW] <= DATAIN;
-	DR1 <= memory[AR1];
-	DR2 <= memory[AR2];
+	if(REG_WRITE) 
+	begin
+		memory[AW] = DATAIN;
+	end
+	DR1 = memory[AR1];
+	DR2 = memory[AR2];
 end
 endmodule
